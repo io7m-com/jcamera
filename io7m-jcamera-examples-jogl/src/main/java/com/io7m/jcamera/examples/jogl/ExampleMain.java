11d85ae2e804636d18194b3e6f0ed4add8e0c7df
Mark Raynsford <code@io7m.com>
2014-10-28 15:54:03 +0000

    Remove frames-per-second reports as this causes performance problems on Windows 7

--- io7m-jcamera-examples-jogl/src/main/java/com/io7m/jcamera/examples/jogl/ExampleMain.java
+++ io7m-jcamera-examples-jogl/src/main/java/com/io7m/jcamera/examples/jogl/ExampleMain.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -106,12 +106,10 @@ public final class ExampleMain
     window.setTitle(ExampleMain.class.getCanonicalName());
 
     /**
-     * Tell the screen to refresh frequently, and to print information about
-     * the current frames-per-second every 60 frames.
+     * Construct an animator to regularly refresh the screen.
      */
 
     final Animator anim = new Animator();
-    anim.setUpdateFPSFrames(60, System.err);
     anim.add(window);
 
     /**
