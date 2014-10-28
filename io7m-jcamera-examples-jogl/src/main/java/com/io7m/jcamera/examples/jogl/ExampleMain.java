29c65c0b975992100db044064eaf4d5c7c4f0a83
Mark Raynsford <code@io7m.com>
2014-10-28 19:41:15 +0000

    Remove call to setTitle until JOGL bug 1098 is resolved

--- io7m-jcamera-examples-jogl/src/main/java/com/io7m/jcamera/examples/jogl/ExampleMain.java
+++ io7m-jcamera-examples-jogl/src/main/java/com/io7m/jcamera/examples/jogl/ExampleMain.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -138,11 +138,6 @@ public final class ExampleMain
       {
         assert drawable != null;
         ++this.frame;
-
-        window.setTitle(String.format(
-          "%s - %f fps",
-          ExampleMain.class.getCanonicalName(),
-          anim.getLastFPS()));
 
         final GL3 g = new DebugGL3(drawable.getGL().getGL3());
         assert g != null;
